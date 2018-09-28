9ea36475542e026b00bda20678024fbc049b1e4f
Bastian Seifert <bastian.seifert@freenet.de>
2018-09-28 11:34:57 +0200

    Added AccessSkill-Skeleton (based on Dice Skill)
    Signed-off-by: Bastian Seifert <bastian.seifert@freenet.de>

---
+++ AccessSkill/AccessSkill/AccessSpeechletRequestStreamHandler.java
@@ -0,0 +1,29 @@
+package dice;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.amazon.speech.speechlet.Speechlet;
+import com.amazon.speech.speechlet.lambda.SpeechletRequestStreamHandler;
+
+
+public class AccessSpeechletRequestStreamHandler extends SpeechletRequestStreamHandler {
+
+    private static final Set<String> supportedApplicationIds;
+
+    static {
+ 
+        supportedApplicationIds = new HashSet<String>();
+        
+    }
+
+    public AccessSpeechletRequestStreamHandler() {
+        super(new DiceSpeechlet(), supportedApplicationIds);
+    }
+
+    public AccessSpeechletRequestStreamHandler(Speechlet speechlet,
+            Set<String> supportedApplicationIds) {
+        super(speechlet, supportedApplicationIds);
+    }
+
+}
